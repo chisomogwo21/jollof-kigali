8721d42db5b45c454db86387f62b93dcffe7941e
Chisomogwo21 <chisomogwo21& 'c:\Users\hp\OneDrive\Desktop\A Solution\index.html'@gmail.com>
2026-03-12 10:57:04 +0200

    Redirect Navbar shopping bag icon to checkout page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -50,7 +50,7 @@ const Navbar: React.FC = () => {
           </div>
 
           <div className="flex max-md:hidden items-center space-x-6">
-            <Link to="/menu" className="relative p-2 text-gray-300 hover:text-gold transition-colors">
+            <Link to="/order" className="relative p-2 text-gray-300 hover:text-gold transition-colors">
               <ShoppingBag size={22} />
               {cartCount > 0 && (
                 <span className="absolute top-0 right-0 bg-gold text-black text-[10px] font-bold h-4 w-4 rounded-full flex items-center justify-center">
@@ -69,7 +69,7 @@ const Navbar: React.FC = () => {
           {/* Mobile Toggle */}
           <div className="md:hidden">
             <div className="flex items-center space-x-4">
-              <Link to="/menu" className="relative p-2 text-gray-300">
+              <Link to="/order" className="relative p-2 text-gray-300">
                 <ShoppingBag size={22} />
                 {cartCount > 0 && (
                   <span className="absolute top-0 right-0 bg-gold text-black text-[10px] font-bold h-4 w-4 rounded-full flex items-center justify-center">
